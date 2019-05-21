dd3073f4345b0528ab35a2ed7a9cc30d88edb2a5
Fer Luna <f.luna096@gmail.com>
2019-05-21 09:04:32 -0500

    Modifique el programa para que ahora incluya todas las letras, recibe una palabra y devuelve una lista con el valor (en braille, para los motores) de cada caracter

--- braille-spa.py
+++ braille-spa.py
@@ -5,20 +5,55 @@ spanish_dict = {
     1: "a",
     2: "b",
     3: "c",
-    4: "d"
+    4: "d",
+    5: "e",
+    6: "f",
+    7: "g",
+    8: "h",
+    9: "i",
+    10: "j",
+    11: "k",
+    12: "l",
+    13: "m",
+    14: "n",
+    15: "ñ",
+    16: "o",
+    17: "p",
+    18: "q",
+    19: "r",
+    20: "s",
+    21: "t",
+    22: "u",
+    23: "v",
+    24: "x",
+    25: "y",
+    26: "z"
 }
-
 word = input()
 character = list(word)
+final_string = []
+cont = 0
 
 
 def converter(word):
+    global cont
     for letter in character:
-        for key, value in spanish_dict.items():  
-            if letter == value: 
-                print ("key: %s , value: %s" % (key, value))
-            else:
-                print ("letter not found")
+        for key, value in spanish_dict.items():
+            if letter == value:
+                print("key: %s , value: %s" % (key, value))
+                final_string.append(key)
+                if (cont == 4):
+                    print("La traducción está limitada a 5 caracteres")
+                    cont = 0
+                    for x in final_string:
+                        print(x)
+                    return final_string
+                    exit()
+                else:
+                    cont = cont + 1
+    cont = 0
+    for x in final_string:
+        print(x)
+
 
 converter(word)
-
