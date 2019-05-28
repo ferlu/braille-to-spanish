0379b72eb8053280dac5c25c16f9c318373ef513
mitkeys <mikeys_zooer@live.com.mx>
2019-05-28 11:23:37 -0500

    Update braille-spa.py

--- braille-spa.py
+++ braille-spa.py
@@ -50,9 +50,11 @@ def converter(word):
         for key, value in spanish_dict.items():
             if letter == value:
                 print("key: %s , value: %s" % (key, value))
+                if key > 26:
+                    final_string.append("#")
                 final_string.append(key)
-                if cont == 3:
-                    print("La traducción está limitada a 4 caracteres")
+                if cont == 4:
+                    print("La traducción está limitada a 5 caracteres")
                     cont = 0
                     for x in final_string:
                         print(x)
