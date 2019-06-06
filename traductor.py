64a357e8a1d45ebd42af9327e1d81892731995d1
Fer Luna <f.luna096@gmail.com>
2019-06-06 00:54:02 -0500

    Validaciones para caracteres especiales de palabras, se optimizo el codigo de la funcion converter, se realizan validaciones para palabras mayores a 5 caracteres y se imprime el resultado de forma correcta [NO PARA LINUX AUN]

--- traductor.py
+++ traductor.py
@@ -3,10 +3,10 @@ import re
 import subprocess
 import os
 import glob
+
 #Variables
 final_string = []
 cont = 0
-
 ourFile = ""
 
 spanish_dict = {
@@ -55,37 +55,14 @@ spanish_dict = {
 def converter(word):
     global cont
     for char in word:
-        if (char == "á"):
-            char = char.replace("á", "a")
-        elif (char == "é"):
-            char = char.replace("é", "e")
-        elif (char == "í"):
-            char = char.replace("í", "i")
-        elif (char == "ó"):
-            char = char.replace("ó", "o")
-        elif (char == "ú"):
-            char = char.replace("ú", "u")
         for key, value in spanish_dict.items():
             if char == value:
                 print("key: %s , value: %s" % (key, value))
                 if key > 26:
                     final_string.append("#")
                 final_string.append(key)
-                if cont == 4:
-                    print("La traducción está limitada a 5 caracteres")
-                    cont = 0
-                    for x in final_string:
-                        print(x)
-                    return final_string
-                    exit()
-                else:
-                    cont += 1
-                    if key > 26:
-                        cont += 1
-
-    cont = 0
-    for x in final_string:
-        print(x)
+                #So we don't have to go through all of our spanish dictionary...
+                break
 
 
 import re
@@ -94,36 +71,35 @@ import os
 from glob import glob
 from subprocess import check_output, CalledProcessError
 
+# def getPath():
+#     global usbNewDir
+#     usbNewDir = ""
 
-def getPath():
-    global usbNewDir
-    usbNewDir = ""
+#     def get_usb_devices():
+#         sdb_devices = map(os.path.realpath, glob('/sys/block/sd*'))
+#         usb_devices = (dev for dev in sdb_devices
+#                        if 'usb' in dev.split('/')[5])
+#         return dict((os.path.basename(dev), dev) for dev in usb_devices)
 
-    def get_usb_devices():
-        sdb_devices = map(os.path.realpath, glob('/sys/block/sd*'))
-        usb_devices = (dev for dev in sdb_devices
-                       if 'usb' in dev.split('/')[5])
-        return dict((os.path.basename(dev), dev) for dev in usb_devices)
+#     def get_mount_points(devices=None):
+#         devices = devices or get_usb_devices(
+#         )  # if devices are None: get_usb_devices
+#         output = check_output(['mount']).splitlines()
+#         is_usb = lambda path: any(dev in path for dev in devices)
+#         usb_info = (line for line in output if is_usb(line.split()[0]))
+#         return [(info.split()[0], info.split()[2]) for info in usb_info]
 
-    def get_mount_points(devices=None):
-        devices = devices or get_usb_devices(
-        )  # if devices are None: get_usb_devices
-        output = check_output(['mount']).splitlines()
-        is_usb = lambda path: any(dev in path for dev in devices)
-        usb_info = (line for line in output if is_usb(line.split()[0]))
-        return [(info.split()[0], info.split()[2]) for info in usb_info]
+#     if __name__ == '__main__':
+#         usb_dir = get_mount_points()
+#         print('Directorio de USB (si la hay):')
+#         print(
+#             usb_dir[0][1])  #Prints always 1st detected usb drive and its path.
+#         usbNewDir = usb_dir[0][1]
+#         #print(usbNewDir)
+#     return (usbNewDir)
 
-    if __name__ == '__main__':
-        usb_dir = get_mount_points()
-        print('Directorio de USB (si la hay):')
-        print(
-            usb_dir[0][1])  #Prints always 1st detected usb drive and its path.
-        usbNewDir = usb_dir[0][1]
-        #print(usbNewDir)
-    return (usbNewDir)
-
-
-usbPath = getPath()
+# usbPath = getPath()
+usbPath = "/Users/ferlu/Documents/CETI/ing/8/proyecto/braille-to-spanish/"
 
 os.chdir(usbPath)
 for document in glob("*.txt"):
@@ -134,16 +110,44 @@ for document in glob("*.txt"):
     else:
         print("No hay archivos válidos")
 
+#reading .txt file from our specified path
 readfile = open(ourFile, "r")
 contents = readfile.read()
 print(contents)
-
+#splitting words using space as a separator
 wordList = contents.split()
+n = 5
+
 for word in wordList:
     for char in word:
+        #Replacing special characters
         if (char == ","):
             word = word.replace(",", "")
         elif (char == "."):
             word = word.replace(".", "")
-    converter(word + " ")
+        elif (char == "á"):
+            word = word.replace("á", "a")
+        elif (char == "é"):
+            word = word.replace("é", "e")
+        elif (char == "í"):
+            word = word.replace("í", "i")
+        elif (char == "ó"):
+            word = word.replace("ó", "o")
+        elif (char == "ú"):
+            word = word.replace("ú", "u")
+
+    #Splitting our word every 5 characters
+    # "holitas" will turn into a list ["holit","as"]
+    splitWord = [word[i:i + n] for i in range(0, len(word), n)]
+    splitWordLength = len(splitWord)
+    #If our split words list has more than one element,
+    #that means our word's length is greater than 5 letters.
+    if (splitWordLength > 1):
+        for word in splitWord:
+            if (word != splitWord[-1]):
+                converter(word)
+            else:
+                converter(word + " ")
+    else:
+        converter(word + " ")
 print(final_string)
