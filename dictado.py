f9d6463df2f62a478ac79cd65adaf852d136bbcb
Fer Luna <f.luna096@gmail.com>
2019-06-07 13:55:40 -0500

    El conversor de voz a texto funciona y almacena las palabras en formato .txt y .docx

---
+++ dictado.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+from __future__ import print_function
+import os
+from pocketsphinx import LiveSpeech, get_model_path
+import re
+import subprocess
+from glob import glob
+from subprocess import check_output, CalledProcessError
+test = open('text.docx', 'w')
+f = open('text.txt', 'w')
+
+print('Bienvenido al conversor de voz a texto para Python.')
+#model_path = get_model_path()
+#print(model_path)
+print('Cargando . . .')
+speech = LiveSpeech(verbose=False,
+                    sampling_rate=16000,
+                    buffer_size=2048,
+                    no_search=False,
+                    full_utt=False,
+                    hmm=os.path.join(model_path, 'es-es'),
+                    lm=os.path.join(model_path, 'es-20k.lm.bin'),
+                    dict=os.path.join(model_path, 'es.dict'))
+print('Por favor di una frase o palabra.    ')
+for phrase in speech:
+    print(phrase)
+    print(phrase, file=test)
+    print(phrase, file=f)
+
+#    phrase.decode()
+#    print(phrase.hypothesis())
